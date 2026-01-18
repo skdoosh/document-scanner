75ea7b0932af853cec5bcd33658a7348a424f54f
Shreyas <149157430+skdoosh@users.noreply.github.com>
2026-01-18 18:33:48 +0530

    feat(scan.py): add argument parser and parse the arguments

--- scan.py
+++ scan.py
@@ -7,3 +7,8 @@ import numpy as np
 from skimage.filters import threshold_local
 
 from pyimagesearch.transform import four_point_transform
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image being scanned")
+args = vars(ap.parse_args)
